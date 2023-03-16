6af1f3ad5c54f41366ee10a02279a27d0c7793fd
Luchopaulucci <luchopaluch@gmail.com>
2023-03-16 02:30:31 -0300

    :sparkles:Se agregan rutas dinamicas

--- TASK3_LucianoPaulucci/assets/script/detail.js
+++ TASK3_LucianoPaulucci/assets/script/detail.js
@@ -1,26 +1,47 @@
-const queryString = location.search  
+const eventosDisponibles = data.events.filter( events => events._id ) 
+console.log(eventosDisponibles);
+
+const eventoNuevo = eventosDisponibles.map( events => {  
+let aux = {}
+    aux._id = events._id
+    aux.image = events.image
+    aux.name = events.name
+    aux.date = events.date
+    aux.description = events.description
+    aux.category = events.category
+    aux.place = events.place
+    aux.capacity = events.capacity
+    return aux
+})
+console.log(eventoNuevo);
+
+const queryString = document.location.search  
 
 const params = new URLSearchParams(queryString) 
 
 const id = params.get("id")
 
-const evento = data.find(event => event._id === _id) 
+const evento = eventoNuevo.find(event => event._id == id) 
+console.log(evento);
 
-const containerCards = document.getElementById("detailHTML") 
+const containerCards = document.getElementById("detailHTML")
 
-
-const div = document.queryselector(".container")
-div.innerHTML = `
+detailHTML.innerHTML = `
 <div class="card" >
-    <div class="row">
+    <div class="row card-detail">
         <div class="col-5 img-card">
-            <img src="${events.image}" class="img-fluid rounded-start" alt="...">
+            <img src="${evento.image}" class="img-fluid " alt="Imagen de evento">
         </div>
-        <div class="col-5  card-text">
+        <div class="col-6  card-text">
             <div class="card-body">
-            <h5>${events.name}</h5>
-            <p>${events.capacity}</p>
+            <h5 class="card-title">${evento.name}</h5>
+            <p class="card-text">Description: ${evento.description}</p>
+            <p class="card-text col-4">Category: ${evento.category}</p>
+            <p class="card-text col-6">Place: ${evento.place}</p>
+            <p class="card-text">Capacity: ${evento.capacity}</p>
             </div>
         </div>
     </div>
 </div>`
+
+
---
+++ TASK3_LucianoPaulucci/assets/script/main.js
@@ -0,0 +1,87 @@
+const eventosIndex = document.getElementById("eventosIndex");
+const checkBox = document.getElementById("checkBox");
+const input = document.querySelector('input')
+
+
+input.addEventListener('input',superFiltro)
+
+checkBox.addEventListener('change',superFiltro)
+
+
+function superFiltro(){
+    let primerFiltro = filtrarPorTexto(data.events,input.value)
+  let segundoFiltro = filtrarPorCheckBox(primerFiltro)
+  mostrarEventos(segundoFiltro)
+}
+
+
+mostrarEventos(data.events);
+mostrarCategorias(data.events);
+
+
+function mostrarCategorias(array) {
+  let arrayCategoriasAll = array.map(events => events.category)
+  
+  let setCategorias = new Set(arrayCategoriasAll)
+  
+  let arrayCategorias = Array.from(setCategorias)
+
+  let categoriasHTML = ``;
+  arrayCategorias.forEach(category => {
+    categoriasHTML += `
+    <div>
+        <input type="checkbox" name="category" id="${category}" value="${category}"/>
+        <label for="${category}">${category}</label>
+    </div>
+    `
+  })
+  checkBox.innerHTML = categoriasHTML;
+}
+
+
+function mostrarEventos(array) {
+  let cardEventos = ``;
+  array.forEach(events => {
+    cardEventos += `
+    <div class="card" style="width: 18rem;">
+      <img src="${events.image}" class="card-img-top" alt="Evento Imagen" style="height: 10rem;">
+        <div class="card-body">
+          <h5 class="card-title">${events.name}</h5>
+          <p class="card-text">${events.description}</p>
+          <div class="btn-card">
+            <p class="card-text flex">Price:$ ${events.price}</p>
+            <a href="./details.html?id=${events._id}" class="btn boton">Go somewhere</a>
+          </div>
+      </div>
+    </div>`
+  })
+  eventosIndex.innerHTML = cardEventos;
+
+  if(array.length === 0){
+    eventosIndex.innerHTML = `<h2">No hay coincidencias <br/> VUELVA A INTENTAR CON OTRA BUSQUEDA</h2>`
+  }
+}
+
+
+function filtrarPorTexto(array,texto){
+  let arrayFiltrado = array.filter(events => events.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayFiltrado
+}
+
+
+function filtrarPorCheckBox(array){
+  let checkBoxs = document.querySelectorAll("input[type='checkbox']")
+
+  let arrayCheck = Array.from(checkBoxs)
+
+  let arrayCheckeados = arrayCheck.filter(check => check.checked)
+
+  let arrayCheckeadosValues = arrayCheckeados.map(checkeados => checkeados.value)
+  
+  let arrayFiltrado = array.filter(elemento => arrayCheckeadosValues.includes(elemento.category))
+
+  if(arrayCheckeados.length > 0){
+    return arrayFiltrado
+  }
+  return array
+}
